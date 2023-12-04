2cc36b67614b82b0accedc371f49fd93cc2558f8
mgrin212 <matthew_grinberg@brown.edu>
2023-12-03 19:24:43 -0500

    Add @vercel/analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/theme-picker";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           <ModeToggle />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
